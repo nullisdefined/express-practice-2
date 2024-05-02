80bc5963f528d278f4ce0199b61c9203b755adfd
nullisdefined <jaeuu.dev@gmail.com>
2024-05-03 00:03:17 +0900

    회원가입, 회원조회 추가

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3333);
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const db = new Map();
+let keyId = 1;
+
+app.get('/', (req, res) => {
+    res.send('main');
+});
+
+// 회원 가입
+app.post('/users', (req, res) => {
+    const userObj = req.body;
+    const userName = userObj.name;
+    const userId = userObj.id;
+    const userPwd = userObj.pwd;
+
+    if(userName && userId && userPwd) {
+        db.set(keyId++, userObj);
+        res.json({ message: `${userName}님, 반갑습니다!` });
+    } else {
+        res.status(400).json({ message: "잘못된 요청입니다." });
+    }
+});
+
+// 로그인
+app.get('/login', (req, res) => {
+    
+});
+
+// 개별 회원 조회
+app.get('/users/:id', (req, res) => {
+    const reqId = parseInt(req.params.id);
+    const userObj = db.get(reqId);
+    
+    if(userObj) {
+        res.json(userObj);
+    } else {
+        res.status(404).json({ message: '잘못된 요청입니다.' })
+    }
+});
+
+// 전체 회원 조회
+app.get('/users', (req, res) => {
+    if(db.size) {
+        res.json(Array.from(db.values()));
+    } else {
+        res.status(404).json({ message: "등록된 회원정보가 없습니다."});
+    }
+});
+
+// 회원 탈퇴
+app.delete('/users/:id', (req, res) => {
+    const reqId = parseInt(req.params.id);
+    const userObj = db.get(reqId);
+    db.delete(reqId);
+    res.json({ message: `${userObj.name}님, 회원탈퇴 되었습니다.` });
+});
+
+app.use((req, res, next) => {
+    res.status(404).send('404 ERROR');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.send('ERROR');
+})
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 서버 대기 중');
+})
