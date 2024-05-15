9ebfd90572a2dbe7bb2fe2fcaf8ef38f1063f93e
nullisdefined <jaeuu.dev@gmail.com>
2024-05-15 10:47:18 +0900

    mariaDB 연동

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',  
+    host: '127.0.0.1',  // localhost 에러 방지
     user: 'root',
     password: 'root',
     database: 'user_management',
